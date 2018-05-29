c65262604f90dc2ba8fc8c4d9af710aef3831028
Dmitry Vakulenko <n0dwis@gmail.com>
2018-05-29 22:29:25 +0300

    Continue making dom parser. Made simple element parsing

--- xsd/dom_parser/parser.go
+++ xsd/dom_parser/parser.go
@@ -6,10 +6,12 @@ import (
 
 type parser struct {
 	types []*Type
+	parseStack *typesStack
 }
 
 func NewParser() *parser {
-	return &parser{}
+	return &parser{
+		parseStack: new(typesStack)}
 }
 
 func (p *parser) LoadFile(fileName string) {
@@ -25,8 +27,10 @@ func (p *parser) LoadFile(fileName string) {
 
 func (p *parser) parseNode(n *dom.Node) {
 	switch n.Name {
-	case "simpleType":
-		p.simpleType(n)
+	//case "simpleType":
+	//	p.simpleType(n)
+	case "element":
+		p.element(n)
 	}
 }
 
@@ -36,8 +40,8 @@ func (p *parser) simpleType(n *dom.Node) {
 		newType := p.createAndAddType(nameAttr.Value)
 		newType.IsSimpleContent = true
 
-		typeAttr  := n.GetAttribute("type")
-		newType.BaseTypeName = p.createAndAddType()
+		//typeAttr  := n.GetAttribute("type")
+		//newType.BaseTypeName = p.createAndAddType()
 	}
 }
 
@@ -48,10 +52,23 @@ func (p *parser) createAndAddType(name string) *Type {
 	return newType
 }
 
-func (p *parser) parseAndCreateQName(name string) QName {
-
-}
+//func (p *parser) parseAndCreateQName(name string) QName {
+//
+//}
 
 func (p *parser) GetTypes() []*Type {
 	return p.types
+}
+
+func (p *parser) element(n *dom.Node) {
+	nameAttr := n.GetAttribute("name")
+	if nameAttr != nil {
+		newType := p.createAndAddType(nameAttr.Value)
+		p.parseStack.Push(newType)
+		if ct := n.GetChild("complexContent"); ct != nil {
+			newType.IsSimpleContent = false
+		} else {
+			newType.IsSimpleContent = true
+		}
+	}
 }
--- xsd/dom_parser/parser_test.go
+++ xsd/dom_parser/parser_test.go
@@ -10,29 +10,36 @@ func TestEmptySchema(t *testing.T) {
 	}
 }
 
-
-func TestSimpleTypes(t *testing.T) {
+func TestSimpleElements(t *testing.T) {
 	typesList := loadXsd(t.Name())
 
 	if len(typesList) != 1 {
 		t.Fatalf("Wrong number of types. 1 expected, but got %d", len(typesList))
 	}
-
-	tp := typesList[0]
-
-	if !tp.IsSimpleContent {
-		t.Fatalf("Type should be complex type")
-	}
-
-	name := "AlphaString_Length1To2"
-	if name != tp.Name {
-		t.Errorf("Field elemName should be %q, got %q instead", name, tp.Name)
-	}
-
-	if tp.BaseTypeName.Name != "string" {
-		t.Errorf("Field should be string, got %q instead", tp.Name)
-	}
 }
+
+//func TestSimpleTypes(t *testing.T) {
+//	typesList := loadXsd(t.Name())
+//
+//	if len(typesList) != 1 {
+//		t.Fatalf("Wrong number of types. 1 expected, but got %d", len(typesList))
+//	}
+//
+//	tp := typesList[0]
+//
+//	if !tp.IsSimpleContent {
+//		t.Fatalf("Type should be complex type")
+//	}
+//
+//	name := "AlphaString_Length1To2"
+//	if name != tp.Name {
+//		t.Errorf("Field elemName should be %q, got %q instead", name, tp.Name)
+//	}
+//
+//	if tp.BaseTypeName.Name != "string" {
+//		t.Errorf("Field should be string, got %q instead", tp.Name)
+//	}
+//}
 
 
 func loadXsd(testName string) []*Type {
